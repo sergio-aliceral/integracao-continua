c256d0161c8c59cdf308557980be97fd06039acd
Sergio Aliceral <sergio.aliceral@gmail.com>
2021-03-24 21:36:43 -0300

    configuração da documentação swagger

--- src/main/java/br/com/aliceraltecnologia/api/resource/ProdutoController.java
+++ src/main/java/br/com/aliceraltecnologia/api/resource/ProdutoController.java
@@ -21,14 +21,18 @@ import org.springframework.web.server.ResponseStatusException;
 import br.com.aliceraltecnologia.api.dto.FiltroProdutoDTO;
 import br.com.aliceraltecnologia.api.dto.ProdutoDTO;
 import br.com.aliceraltecnologia.api.form.ProdutoForm;
+import br.com.aliceraltecnologia.config.SwaggerConfig;
 import br.com.aliceraltecnologia.model.entity.Produto;
 import br.com.aliceraltecnologia.model.filter.ProdutoFilter;
 import br.com.aliceraltecnologia.service.ProdutoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("products")
+@Api(tags = { SwaggerConfig.PRODUTOS })
 public class ProdutoController {
 
 	private final ProdutoService service;
@@ -37,6 +41,7 @@ public class ProdutoController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
+	@ApiOperation("Cria um produto")
 	public ProdutoDTO salva(@RequestBody @Valid ProdutoForm form) {
 		Produto produto = modelMapper.map(form, Produto.class);
 		produto = service.salva(produto);
@@ -44,6 +49,7 @@ public class ProdutoController {
 	}
 
 	@GetMapping("{id}")
+	@ApiOperation("Busca o produto pelo id")
 	public ProdutoDTO buscaPorId(@PathVariable Long id) {
 		return service
 				.buscaPorId(id)
@@ -52,12 +58,14 @@ public class ProdutoController {
 	}
 	
 	@DeleteMapping("{id}")
+	@ApiOperation("Exclui o produto")
 	public void exclui(@PathVariable Long id) {
 		Produto produto = service.buscaPorId(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 		service.exclui(produto);
 	}
 	
 	@PutMapping("{id}")
+	@ApiOperation("Altera um produto")
 	public ProdutoDTO altera(@PathVariable Long id, @RequestBody @Valid ProdutoForm form) {
 		return service.buscaPorId(id).map( produto -> {
 			
@@ -72,6 +80,7 @@ public class ProdutoController {
 	}
 	
 	@GetMapping
+	@ApiOperation("Lista os produtos")
 	public List<ProdutoDTO> lista() {
 		return service.lista().stream()
 				.map(produto -> modelMapper.map(produto, ProdutoDTO.class))
@@ -79,6 +88,7 @@ public class ProdutoController {
 	}
 	
 	@GetMapping("search")
+	@ApiOperation("Pesquisa os produtos pelo filtro")
 	public List<ProdutoDTO> pesquisa(FiltroProdutoDTO dto) {
 		ProdutoFilter produtoFilter = modelMapper.map(dto, ProdutoFilter.class);
 		return service.pesquisa(produtoFilter).stream()
